b2c8b41dab8c2af324e6c0784ecb70426e19bb3d
A-m79 <tacoslebg123@gmail.com>
2026-07-25 09:55:13 +0200

    last fix absences

--- commands/liste-absences.js
+++ commands/liste-absences.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('absences')
+        .setName('liste-absences')
         .setDescription('📋 Affiche la liste récapitulative des absences et leur statut'),
 
     async execute(interaction) {
