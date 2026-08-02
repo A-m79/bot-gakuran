9f50aefcf1f75bc0cc49ba0de92f9a95efc5a07f
A-m79 <tacoslebg123@gmail.com>
2026-08-02 22:37:14 +0200

    Fix: Intégration définitive du GIF Tokyo Revengers et texte exact

--- commands/setup-ticket.js
+++ commands/setup-ticket.js
@@ -12,11 +12,11 @@ module.exports = {
             .setColor('#FF2A7A')
             .setDescription(
                 '# 💡 Une question, un report ou une suggestion ?\n' +
-                'Un problème en jeu, une réclamation ou une idée à partager avec le gang ? N\'hésite pas à nous en faire part.\n\n' +
-                '> 📌 **Clique sur le bouton ci-dessous** pour ouvrir un salon de discussion privé et sécurisé avec l\'équipe.\n\n' +
+                'N\'hésite pas à nous en faire part.\n\n' +
+                '> 📌 **Afin d\'ouvrir un ticket, clique sur le bouton ci-dessous.**\n\n' +
                 '```ansi\n[31m⚠️ Tout ticket ouvert pour troll ou abus entraînera une sanction.\n```'
             )
-            .setImage('https://media1.giphy.com/media/uZZVUsUSWxtdpeQoMC/giphy.gif')
+            .setImage('https://cdn.discordapp.com/attachments/1525294245236445226/1533574234406781138/8eab73c05066210554068a5cd4d5fea9.gif?ex=6a70fbcd&is=6a6faa4d&hm=edf5c9c7b50b95596b166e277cfb779b18db9f8876cf50886b52edc4f3b32d38&')
             .setFooter({ text: 'Gurenkai Gang • Système de Tickets', iconURL: interaction.guild.iconURL({ dynamic: true }) });
 
         const button = new ButtonBuilder()
